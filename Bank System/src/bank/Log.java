e7668a6fd8be01267eb045d633e472d831d49bb3
evgenika033 <63950312+evgenika033@users.noreply.github.com>
2020-04-19 12:47:05 +0300

    create project bank system create classes: Account Bank Client Log Logger

---
+++ Bank System/src/bank/Log.java
@@ -0,0 +1,25 @@
+package bank;
+
+public class Log {
+	private long timestamp;
+	private int clientId;
+	private String description;
+	private float amount;
+	
+	public Log(long timestamp, int clientId, String description, float amount) {
+		super();
+		this.timestamp = timestamp;
+		this.clientId = clientId;
+		this.description = description;
+		this.amount = amount;
+	}
+	
+	public String getData() {
+		return "";
+	}
+		
+	
+		
+	
+
+}
---
+++ Bank System/src/bank/Logger.java
@@ -0,0 +1,19 @@
+package bank;
+
+public class Logger {
+	private String driverName;
+
+	public Logger(String driverName) {
+		super();
+		this.driverName = driverName;
+		
+	}
+	//implement to print Log (פעולה)on screen
+	public void log(Log log) {
+		
+	}
+	
+	public Log[] getLogs() {
+		return null;
+	}
+}
